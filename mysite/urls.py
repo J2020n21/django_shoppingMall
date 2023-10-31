b7b649a884464654831eb81500f5704b0f61e739
Jiui <jiui0103@naver.com>
2023-10-31 15:26:55 +0900

    1.make comment function- CRUD 2. fix admin page better display and add search

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('detail/<int:content_id>/',views.detail, name='detail')
+    path('detail/<int:content_id>/',views.detail, name='detail'),
+    path('comment/create/<int:content_id>/', views.comment_create, name='comment_create'),
+    path('comment/update/<int:comment_id>/', views.comment_update, name='comment_update'),
+    path('comment/delete/<int:comment_id>/', views.comment_delete, name='comment_delete'),
+
 ]
